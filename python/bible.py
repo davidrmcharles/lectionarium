9837454a962e412353bda012fc8af55d2da1a0b1
davidrmcharles <31576489+davidrmcharles@users.noreply.github.com>
2017-09-08 13:03:04 -0700

    The Bible object now has all the text.  Each Book object contains its respective text in an ordered dictionary keyed by Point objects.  The loading of the text depends upon the verse-parsing assets, which currently have no other dependencies.  These I have moved to the top of the file.  Consider outfactoring them.

--- python/bible.py
+++ python/bible.py
@@ -2,253 +2,35 @@
 '''
 Represents the Canon of Scripture
 
+Verse References
+======================================================================
+
+* :class:`Point`
+* :class:`Range`
+* :func:`_parseVersesToken`
+
+Everything Else
+======================================================================
+
 * :class:`Ref`
 * :func:`parseRef`
 * :func:`_parseBookTokensGreedily`
 * :class:`Book`
 * :class:`Bible`
 * :func:`_parseBookToken`
-* :class:`Point`
-* :class:`Range`
-* :func:`_parseVersesToken`
 '''
 
-class Ref(object):
-    '''
-    Represents a range of text in a particular scriptural book.
-    '''
+# Standard imports:
+import collections
+import inspect
+import os
+import sys
+import traceback
 
-    def __init__(self, book, verses):
-        self.book = book
-        self.verses = verses
-
-def parseRef(text):
-    '''
-    Parse a human-readable, single-book bible reference and return the
-    result as a :class:`Ref`.
-    '''
-
-    # Fail if `text` is not a string.
-    if not isinstance(text, basestring):
-        raise TypeError(
-            'Non-string (%s, %s) passed to parseRef()!' % (
-                type(text), text))
-
-    # Fail if there are no non-white characters in `text`.
-    tokens = text.strip().split()
-    if len(tokens) == 0:
-        raise ValueError(
-            'No non-white characters passed to parseRef()!')
-
-    # Try to parse a book out of the leading tokens in a 'greedy'
-    # fashion.
-    book, tokensConsumed = _parseBookTokensGreedily(tokens)
-
-    # If there is more than one token remaining, we have too many
-    # tokens.
-    remainingTokens = tokens[tokensConsumed:]
-    if len(remainingTokens) > 1:
-        raise ValueError(
-            'Extra tokens %s!' % remainingTokens[1:])
-
-    # If there is exactly remaining token, try to parse verses out of
-    # it.
-    verses = None
-    if len(remainingTokens) == 1:
-        remainingToken = remainingTokens[0]
-        verses = _parseVersesToken(remainingToken)
-
-    return Ref(book, verses)
-
-def _parseBookTokensGreedily(tokens):
-    '''
-    Return the book represented by the leading tokens and the number
-    of tokens consumed.  If no book can be parsed out of the tokens,
-    return ``(None, 0)``.
-
-    This function parses in a 'greedy' fashion, trying to consume as
-    many tokens as possible.  This is key to parsing 'Song of Songs',
-    whose abbreviations include 'Song'.
-    '''
-
-    for index in reversed(range(len(tokens))):
-        superToken = ''.join(tokens[:index + 1])
-        book = _parseBookToken(superToken)
-        if book is not None:
-            return book, index + 1
-    return None, 0
-
-class Book(object):
-    '''
-    Represents a single scriptural 'book'.
-    '''
-
-    def __init__(self, name, *abbreviations):
-        self.name = name
-        self.abbreviations = list(abbreviations)
-
-    @property
-    def normalName(self):
-        '''
-        The normalized name for the book:
-
-        * Full-book name (no abbreviations)
-        * All lowercase
-        * No interior whitespace
-        '''
-
-        return ''.join(self.name.lower().split())
-
-    @property
-    def normalAbbreviations(self):
-        '''
-        The abbreviations for the book, normalized.
-
-        (Same rules as for ``normalName``.
-        '''
-
-        return [
-            ''.join(abbreviation.lower().split())
-            for abbreviation in self.abbreviations
-            ]
-
-    def matchesToken(self, token):
-        '''
-        Return ``True`` if `token` can refer to this book.
-        '''
-
-        token = ''.join(token.lower().split())
-        return token in [self.normalName] + self.normalAbbreviations
-
-    def __str__(self):
-        return '%s (%s)' % (
-            self.name,
-            ', '.join(self.abbreviations))
-
-    def __repr__(self):
-        return '<bible.Book object "%s" at 0x%x>' % (
-            self, id(self))
-
-class Bible(object):
-    '''
-    Represents the whole Canon of Scripture.
-    '''
-
-    def __init__(self):
-        self._otBooks = [
-            Book('Genesis', 'Gn'),
-            Book('Exodus', 'Ex'),
-            Book('Leviticus', 'Lv'),
-            Book('Numbers', 'Nm'),
-            Book('Deuteronomy', 'Dt'),
-            Book('Joshua', 'Jos'),
-            Book('Judges', 'Jgs'),
-            Book('Ruth', 'Ru'),
-            Book('1 Samuel', '1 Sm'),
-            Book('2 Samuel', '2 Sm'),
-            Book('1 Kings', '1 Kgs'),
-            Book('2 Kings', '2 Kgs'),
-            Book('1 Chronicles', '1 Chr'),
-            Book('2 Chronicles', '2 Chr'),
-            Book('Ezra', 'Ezr'),
-            Book('Nehemiah', 'Neh'),
-            Book('Tobit', 'Tb'),
-            Book('Judith', 'Jdt'),
-            Book('Esther', 'Est'),
-            Book('1 Maccabees', '1 Mc'),
-            Book('2 Maccabees', '2 Mc'),
-            Book('Job', 'Jb'),
-            Book('Psalms', 'Ps', 'Pss'),
-            Book('Proverbs', 'Prv'),
-            Book('Ecclesiastes', 'Eccl'),
-            Book('Song of Songs', 'Song', 'Sg'),
-            Book('Wisdom', 'Wis'),
-            Book('Sirach', 'Sir'),
-            Book('Isaiah', 'Is'),
-            Book('Jeremiah', 'Jr'),
-            Book('Lamentations', 'Lam'),
-            Book('Baruch', 'Bar'),
-            Book('Ezekiel', 'Ez'),
-            Book('Daniel', 'Dn'),
-            Book('Hosea', 'Hos'),
-            Book('Joel', 'Jl'),
-            Book('Amos', 'Am'),
-            Book('Obadiah', 'Ob'),
-            Book('Jonah', 'Jon'),
-            Book('Michah', 'Mi'),
-            Book('Nahum', 'Na'),
-            Book('Habakkuk', 'Hb'),
-            Book('Zephaniah', 'Zep'),
-            Book('Haggai', 'Hg'),
-            Book('Zechariah', 'Zec'),
-            Book('Malachi', 'Mal'),
-            ]
-
-        self._ntBooks = [
-            Book('Matthew', 'Mt'),
-            Book('Mark', 'Mk'),
-            Book('Luke', 'Lk'),
-            Book('John', 'Jn'),
-            Book('Acts', 'Acts'),
-            Book('Romans', 'Rom'),
-            Book('1 Corinthians', '1 Cor'),
-            Book('2 Corinthians', '2 Cor'),
-            Book('Galatians', 'Gal'),
-            Book('Ephesians', 'Eph'),
-            Book('Philippians', 'Phil'),
-            Book('Colossians', 'Col'),
-            Book('1 Thessalonians', '1 Thes'),
-            Book('2 Thessalonians', '2 Thes'),
-            Book('1 Timothy', '1 Tm'),
-            Book('2 Timothy', '2 Tm'),
-            Book('Titus', 'Ti'),
-            Book('Philemon', 'Phlm'),
-            Book('Hebrews', 'Heb'),
-            Book('James', 'Jas'),
-            Book('1 Peter', '1 Pt'),
-            Book('2 Peter', '2 Pt'),
-            Book('1 John', '1 Jn'),
-            Book('2 John', '2 Jn'),
-            Book('3 John', '3 Jn'),
-            Book('Jude', 'Jude'),
-            Book('Revelation', 'Rv'),
-            ]
-
-        self._allBooks = self._otBooks + self._ntBooks
-
-    @property
-    def allBooks(self):
-        '''
-        All books.
-        '''
-
-        return self._allBooks
-
-    def findBook(self, token):
-        '''
-        Find and return the book that goes with `token`.
-        '''
-
-        for book in self._allBooks:
-            if book.matchesToken(token):
-                return book
-        return None
-
-_bible = Bible()
-
-def _parseBookToken(token):
-    '''
-    Parse a book token in any form to a normalized form:
-
-    * Full-book name (no abbreviation)
-    * All lowercase
-    * Interior whitespace removed
-    '''
-
-    book = _bible.findBook(token)
-    if book is None:
-        return None
-    return book.normalName
+# For the moment, assume that the text is in '../../myclemtext'.
+_thisFilePath = inspect.getfile(inspect.currentframe())
+_projectFolderPath = os.path.dirname(os.path.dirname(_thisFilePath))
+_textFolderPath = os.path.join(_projectFolderPath, 'myclemtext')
 
 class Point(object):
     '''
@@ -406,3 +188,268 @@ def _parseVersesToken(token):
             result.append(Range(first, second))
 
     return result
+
+class Ref(object):
+    '''
+    Represents a range of text in a particular scriptural book.
+    '''
+
+    def __init__(self, book, verses):
+        self.book = book
+        self.verses = verses
+
+def parseRef(text):
+    '''
+    Parse a human-readable, single-book bible reference and return the
+    result as a :class:`Ref`.
+    '''
+
+    # Fail if `text` is not a string.
+    if not isinstance(text, basestring):
+        raise TypeError(
+            'Non-string (%s, %s) passed to parseRef()!' % (
+                type(text), text))
+
+    # Fail if there are no non-white characters in `text`.
+    tokens = text.strip().split()
+    if len(tokens) == 0:
+        raise ValueError(
+            'No non-white characters passed to parseRef()!')
+
+    # Try to parse a book out of the leading tokens in a 'greedy'
+    # fashion.
+    book, tokensConsumed = _parseBookTokensGreedily(tokens)
+
+    # If there is more than one token remaining, we have too many
+    # tokens.
+    remainingTokens = tokens[tokensConsumed:]
+    if len(remainingTokens) > 1:
+        raise ValueError(
+            'Extra tokens %s!' % remainingTokens[1:])
+
+    # If there is exactly remaining token, try to parse verses out of
+    # it.
+    verses = None
+    if len(remainingTokens) == 1:
+        remainingToken = remainingTokens[0]
+        verses = _parseVersesToken(remainingToken)
+
+    return Ref(book, verses)
+
+def _parseBookTokensGreedily(tokens):
+    '''
+    Return the book represented by the leading tokens and the number
+    of tokens consumed.  If no book can be parsed out of the tokens,
+    return ``(None, 0)``.
+
+    This function parses in a 'greedy' fashion, trying to consume as
+    many tokens as possible.  This is key to parsing 'Song of Songs',
+    whose abbreviations include 'Song'.
+    '''
+
+    for index in reversed(range(len(tokens))):
+        superToken = ''.join(tokens[:index + 1])
+        book = _parseBookToken(superToken)
+        if book is not None:
+            return book, index + 1
+    return None, 0
+
+class Book(object):
+    '''
+    Represents a single scriptural 'book'.
+    '''
+
+    def __init__(self, name, *abbreviations):
+        self.name = name
+        self.abbreviations = list(abbreviations)
+        self.textByVerse = collections.OrderedDict()
+
+    @property
+    def normalName(self):
+        '''
+        The normalized name for the book:
+
+        * Full-book name (no abbreviations)
+        * All lowercase
+        * No interior whitespace
+        '''
+
+        return ''.join(self.name.lower().split())
+
+    @property
+    def normalAbbreviations(self):
+        '''
+        The abbreviations for the book, normalized.
+
+        (Same rules as for ``normalName``.
+        '''
+
+        return [
+            ''.join(abbreviation.lower().split())
+            for abbreviation in self.abbreviations
+            ]
+
+    def matchesToken(self, token):
+        '''
+        Return ``True`` if `token` can refer to this book.
+        '''
+
+        token = ''.join(token.lower().split())
+        return token in [self.normalName] + self.normalAbbreviations
+
+    def __str__(self):
+        return '%s (%s)' % (
+            self.name,
+            ', '.join(self.abbreviations))
+
+    def __repr__(self):
+        return '<bible.Book object "%s" at 0x%x>' % (
+            self, id(self))
+
+    def loadText(self):
+        textFileName = '%s.txt' % self.normalName
+        textFilePath = os.path.join(_textFolderPath, textFileName)
+        with open(textFilePath, 'r') as inputFile:
+            for line in inputFile.readlines():
+                verseToken, text = line.split(' ', 1)
+                verseList = _parseVersesToken(verseToken)
+                verse = verseList[0]
+                self.textByVerse[verse] = text.strip()
+
+    def printText(self, outputFile=sys.stdout):
+        for verseKey, verseText in self.textByVerse.iteritems():
+            outputFile.write('%s %s\n' % (verseKey, verseText))
+
+class Bible(object):
+    '''
+    Represents the whole Canon of Scripture.
+    '''
+
+    def __init__(self):
+        self._otBooks = [
+            Book('Genesis', 'Gn'),
+            Book('Exodus', 'Ex'),
+            Book('Leviticus', 'Lv'),
+            Book('Numbers', 'Nm'),
+            Book('Deuteronomy', 'Dt'),
+            Book('Joshua', 'Jos'),
+            Book('Judges', 'Jgs'),
+            Book('Ruth', 'Ru'),
+            Book('1 Samuel', '1 Sm'),
+            Book('2 Samuel', '2 Sm'),
+            Book('1 Kings', '1 Kgs'),
+            Book('2 Kings', '2 Kgs'),
+            Book('1 Chronicles', '1 Chr'),
+            Book('2 Chronicles', '2 Chr'),
+            Book('Ezra', 'Ezr'),
+            Book('Nehemiah', 'Neh'),
+            Book('Tobit', 'Tb'),
+            Book('Judith', 'Jdt'),
+            Book('Esther', 'Est'),
+            Book('1 Maccabees', '1 Mc'),
+            Book('2 Maccabees', '2 Mc'),
+            Book('Job', 'Jb'),
+            Book('Psalms', 'Ps', 'Pss'),
+            Book('Proverbs', 'Prv'),
+            Book('Ecclesiastes', 'Eccl'),
+            Book('Song of Songs', 'Song', 'Sg'),
+            Book('Wisdom', 'Wis'),
+            Book('Sirach', 'Sir'),
+            Book('Isaiah', 'Is'),
+            Book('Jeremiah', 'Jr'),
+            Book('Lamentations', 'Lam'),
+            Book('Baruch', 'Bar'),
+            Book('Ezekiel', 'Ez'),
+            Book('Daniel', 'Dn'),
+            Book('Hosea', 'Hos'),
+            Book('Joel', 'Jl'),
+            Book('Amos', 'Am'),
+            Book('Obadiah', 'Ob'),
+            Book('Jonah', 'Jon'),
+            Book('Michah', 'Mi'),
+            Book('Nahum', 'Na'),
+            Book('Habakkuk', 'Hb'),
+            Book('Zephaniah', 'Zep'),
+            Book('Haggai', 'Hg'),
+            Book('Zechariah', 'Zec'),
+            Book('Malachi', 'Mal'),
+            ]
+
+        self._ntBooks = [
+            Book('Matthew', 'Mt'),
+            Book('Mark', 'Mk'),
+            Book('Luke', 'Lk'),
+            Book('John', 'Jn'),
+            Book('Acts', 'Acts'),
+            Book('Romans', 'Rom'),
+            Book('1 Corinthians', '1 Cor'),
+            Book('2 Corinthians', '2 Cor'),
+            Book('Galatians', 'Gal'),
+            Book('Ephesians', 'Eph'),
+            Book('Philippians', 'Phil'),
+            Book('Colossians', 'Col'),
+            Book('1 Thessalonians', '1 Thes'),
+            Book('2 Thessalonians', '2 Thes'),
+            Book('1 Timothy', '1 Tm'),
+            Book('2 Timothy', '2 Tm'),
+            Book('Titus', 'Ti'),
+            Book('Philemon', 'Phlm'),
+            Book('Hebrews', 'Heb'),
+            Book('James', 'Jas'),
+            Book('1 Peter', '1 Pt'),
+            Book('2 Peter', '2 Pt'),
+            Book('1 John', '1 Jn'),
+            Book('2 John', '2 Jn'),
+            Book('3 John', '3 Jn'),
+            Book('Jude', 'Jude'),
+            Book('Revelation', 'Rv'),
+            ]
+
+        self._allBooks = self._otBooks + self._ntBooks
+        self._loadText()
+
+    @property
+    def allBooks(self):
+        '''
+        All books.
+        '''
+
+        return self._allBooks
+
+    def findBook(self, token):
+        '''
+        Find and return the book that goes with `token`.
+        '''
+
+        for book in self._allBooks:
+            if book.matchesToken(token):
+                return book
+        return None
+
+    def findText(self, ref):
+        '''
+        Find and return the text that goes with `ref`.
+        '''
+
+        book = self.findBook(ref.book)
+        return book.findText(ref.verses)
+
+    def _loadText(self):
+        for book in self.allBooks:
+            book.loadText()
+
+_bible = Bible()
+
+def _parseBookToken(token):
+    '''
+    Parse a book token in any form to a normalized form:
+
+    * Full-book name (no abbreviation)
+    * All lowercase
+    * Interior whitespace removed
+    '''
+
+    book = _bible.findBook(token)
+    if book is None:
+        return None
+    return book.normalName
