bc1d044d76c54adb2124259a9c83956842fb3812
davidrmcharles <31576489+davidrmcharles@users.noreply.github.com>
2018-07-18 19:10:28 -0700

    Adding test stubs for module `datetools`.

---
+++ python/test/test_datetools.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+'''
+Tests for :mod:`datetools`
+'''
+
+# Standard imports
+import unittest
+
+# Local imports:
+import domtools
+
+class nextSunday(unittest.TestCase):
+    pass  # TODO
+
+class followingDays(unittest.TestCase):
+    pass  # TODO
+
+class inclusiveDateRange(unittest.TestCase):
+    pass  # TODO
+
+class dateOfEaster(unittest.TestCase):
+    pass  # TODO
+
+class sundayCycleForDate(unittest.TestCase):
+    pass  # TODO
+
+class weekdayCycleForDate(unittest.TestCase):
+    pass  # TODO
+
+if __name__ == '__main__':
+    unittest.main()
